9baffd347257742b5806bf227034b18d6b6773e1
Chris Norris <chris@bobandchris.co.uk>
2015-04-20 14:24:08 +0100

    bug testing new commands handler + modulized commands

--- core/commands/version.tcl
+++ core/commands/version.tcl
@@ -1,7 +1,7 @@
 service::commands::register version -1 [namespace current]::version_cmd
 
 proc version_cmd {nickname hostname handle channel text} {
-	global lastbind
+	global lastbind; variable linecount
 	helper_xtra_set "lastcmd" $handle "$channel $lastbind $text"
 	set modules [loadedmodules]
 	puthelp "NOTICE $nickname :[getconf core script]: [getconf core version]_[getconf core verstxt] by [getconf core author] loaded! (Line Count: $linecount) ([llength $modules] module(s) loaded[expr {[llength $modules]>0  ? ": [join $modules ", "]" : ""}])."
